4a29cf267b18216c44ba504a49c518022f88c553
Gbolahan Odumuyiwa <ishaqgbo@gmail.com>
2018-02-20 16:38:20 +0100

    ACME Power hackthaton updated codes

--- acme-power-outages.py
+++ acme-power-outages.py
@@ -6,7 +6,7 @@ next(reader, None)
 for row in reader:
     k, v = row
     ap_surge[k] = v
-#print ap_surge
+
 
 #converting data in dict from strings to integers
 ap_surge_int ={}
@@ -15,14 +15,12 @@ for key in ap_surge.keys():
     int_values =float(ap_surge[key])
     ap_surge_int[int_key] =int_values
     #print(type(ap_surge_int[int_key]))
-
 #print(ap_surge_int)
 
-#checking for surges in data in the ap_surge dict
+## checking for surges in data in the ap_surge dict
 
 normal_power_threshold = 4.9
 count = 0
-acount = 0
 for readings in ap_surge_int.values():
     if readings > normal_power_threshold:
         count = count + 1
